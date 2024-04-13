717c6b836d153612e824a58947a92e2f84e4d40b
jesanchez1 <jesanchez@virfon.com>
2024-04-13 18:12:14 -0400

    initial commit

---
+++ javascript/data.js
@@ -0,0 +1,204 @@
+let data = {
+    "teams": [
+    {
+      "rank": 1,
+      "name": "Arsenal",
+      "played": 31,
+      "won": 22,
+      "drawn": 5,
+      "lost": 4,
+      "goalsDifference": "+51",
+      "points": 71
+    },
+    {
+      "rank": 2,
+      "name": "Liverpool",
+      "played": 31,
+      "won": 21,
+      "drawn": 8,
+      "lost": 2,
+      "goalsDifference": "+42",
+      "points": 71
+    },
+    {
+      "rank": 3,
+      "name": "Manchester City",
+      "played": 31,
+      "won": 20,
+      "drawn": 7,
+      "lost": 4,
+      "goalsDifference": "+46",
+      "points": 67
+    },
+    {
+      "rank": 4,
+      "name": "Manchester United",
+      "played": 31,
+      "won": 19,
+      "drawn": 9,
+      "lost": 3,
+      "goalsDifference": "+38",
+      "points": 66
+    },
+    {
+      "rank": 5,
+      "name": "Chelsea",
+      "played": 31,
+      "won": 17,
+      "drawn": 10,
+      "lost": 4,
+      "goalsDifference": "+29",
+      "points": 61
+    },
+    {
+      "rank": 6,
+      "name": "Leicester City",
+      "played": 31,
+      "won": 17,
+      "drawn": 6,
+      "lost": 8,
+      "goalsDifference": "+23",
+      "points": 57
+    },
+    {
+      "rank": 7,
+      "name": "West Ham United",
+      "played": 31,
+      "won": 16,
+      "drawn": 8,
+      "lost": 7,
+      "goalsDifference": "+10",
+      "points": 56
+    },
+    {
+      "rank": 8,
+      "name": "Tottenham Hotspur",
+      "played": 31,
+      "won": 15,
+      "drawn": 8,
+      "lost": 8,
+      "goalsDifference": "+20",
+      "points": 53
+    },
+    {
+      "rank": 9,
+      "name": "Everton",
+      "played": 31,
+      "won": 15,
+      "drawn": 7,
+      "lost": 9,
+      "goalsDifference": "+3",
+      "points": 52
+    },
+    {
+      "rank": 10,
+      "name": "Leeds United",
+      "played": 31,
+      "won": 14,
+      "drawn": 5,
+      "lost": 12,
+      "goalsDifference": "-2",
+      "points": 47
+    },
+    {
+      "rank": 11,
+      "name": "Aston Villa",
+      "played": 31,
+      "won": 13,
+      "drawn": 6,
+      "lost": 12,
+      "goalsDifference": "+8",
+      "points": 45
+    },
+    {
+      "rank": 12,
+      "name": "Wolverhampton Wanderers",
+      "played": 31,
+      "won": 11,
+      "drawn": 8,
+      "lost": 12,
+      "goalsDifference": "-6",
+      "points": 41
+    },
+    {
+      "rank": 13,
+      "name": "Crystal Palace",
+      "played": 31,
+      "won": 10,
+      "drawn": 8,
+      "lost": 13,
+      "goalsDifference": "-15",
+      "points": 38
+    },
+    {
+      "rank": 14,
+      "name": "Newcastle United",
+      "played": 31,
+      "won": 9,
+      "drawn": 8,
+      "lost": 14,
+      "goalsDifference": "-18",
+      "points": 35
+    },
+    {
+      "rank": 15,
+      "name": "Brighton & Hove Albion",
+      "played": 31,
+      "won": 7,
+      "drawn": 12,
+      "lost": 12,
+      "goalsDifference": "-5",
+      "points": 33
+    },
+    {
+      "rank": 16,
+      "name": "Burnley",
+      "played": 31,
+      "won": 8,
+      "drawn": 9,
+      "lost": 14,
+      "goalsDifference": "-18",
+      "points": 33
+    },
+    {
+      "rank": 17,
+      "name": "Southampton",
+      "played": 31,
+      "won": 8,
+      "drawn": 8,
+      "lost": 15,
+      "goalsDifference": "-17",
+      "points": 32
+    },
+    {
+      "rank": 18,
+      "name": "Watford",
+      "played": 31,
+      "won": 7,
+      "drawn": 10,
+      "lost": 14,
+      "goalsDifference": "-21",
+      "points": 31
+    },
+    {
+      "rank": 19,
+      "name": "Norwich City",
+      "played": 31,
+      "won": 6,
+      "drawn": 9,
+      "lost": 16,
+      "goalsDifference": "-23",
+      "points": 27
+    },
+    {
+      "rank": 20,
+      "name": "Sheffield United",
+      "played": 31,
+      "won": 3,
+      "drawn": 7,
+      "lost": 21,
+      "goalsDifference": "-52",
+      "points": 16
+    }
+  ]
+}  
---
+++ javascript/fill.js
@@ -0,0 +1,21 @@
+function fill() {
+    console.log("starting");
+    for (let i = 0; i < 20; i++) {
+        console.log(data["teams"][i]["rank"]);
+
+        let row = document.getElementById('table').rows[data["teams"][i]["rank"]];
+
+        row.cells[0].innerHTML = data["teams"][i]["rank"];
+        row.cells[1].innerHTML = '<a href="teams/'+data["teams"][i]["name"].toLowerCase()+'.html">'+ data["teams"][i]["name"] + "</a>";  
+        row.cells[2].innerHTML = data["teams"][i]["played"];
+        row.cells[3].innerHTML = data["teams"][i]["won"];
+        row.cells[4].innerHTML = data["teams"][i]["drawn"];
+        row.cells[5].innerHTML = data["teams"][i]["lost"];  
+        row.cells[6].innerHTML = data["teams"][i]["points"]; 
+
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    fill();
+});
