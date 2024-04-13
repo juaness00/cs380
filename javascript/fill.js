c22a9eb51b1135b14438c8d9d63ff7bc8bb09a0a
jesanchez1 <jesanchez@virfon.com>
2024-04-13 18:22:59 -0400

    added replace

--- javascript/fill.js
+++ javascript/fill.js
@@ -1,12 +1,12 @@
-function fill() {
+function fill(){
     console.log("starting");
-    for (let i = 0; i < 20; i++) {
+    for(let i = 0; i < 20; i++){
         console.log(data["teams"][i]["rank"]);
 
         let row = document.getElementById('table').rows[data["teams"][i]["rank"]];
 
         row.cells[0].innerHTML = data["teams"][i]["rank"];
-        row.cells[1].innerHTML = '<a href="teams/'+data["teams"][i]["name"].toLowerCase()+'.html">'+ data["teams"][i]["name"] + "</a>";  
+        row.cells[1].innerHTML = '<a href="teams/'+data["teams"][i]["name"].toLowerCase()+'.html">'+ data["teams"][i]["name"].replace("_", " ") + "</a>";  
         row.cells[2].innerHTML = data["teams"][i]["played"];
         row.cells[3].innerHTML = data["teams"][i]["won"];
         row.cells[4].innerHTML = data["teams"][i]["drawn"];
@@ -16,6 +16,6 @@ function fill() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded",function(){
     fill();
 });
